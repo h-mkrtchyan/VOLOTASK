f69cec26d4720c9fa601a50b867f8e961b2e01ca
h-mkrtchyan <hakobmkrtchyann@gmail.com>
2016-10-02 23:59:58 +0400

    fixes

---
+++ BookStore/StoreBooksMVC/Controllers/GenresController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+
+namespace StoreBooksMVC.Controllers
+{
+    public class GenresController : Controller
+    {
+        private BookStoreEntities db = new BookStoreEntities();
+
+        // GET: Genres
+        public async Task<ActionResult> Index(string sortedQuery, string currentFilter)
+        {
+            ViewBag.CurrentSort = sortedQuery;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortedQuery) ? "GenreName—desc" : "";
+
+            var genre = from g in db.Genres select g;
+
+            switch (sortedQuery)
+            {
+                default:
+                    genre = genre.OrderBy(g => g.GenreName);
+                    break;
+            }
+
+            return View(await db.Genres.ToListAsync());
+        }
+
+        // GET: Genres/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = await db.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,GenreName")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genres/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = await db.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genres/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,GenreName")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = await db.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Genre genre = await db.Genres.FindAsync(id);
+            db.Genres.Remove(genre);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

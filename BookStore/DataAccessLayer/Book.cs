d4eb950c81195661b811ea2ddc37e6da0c225849
h-mkrtchyan <hakobmkrtchyann@gmail.com>
2016-10-16 13:15:08 +0400

    unknown commit :)

--- BookStore/DataAccessLayer/Book.cs
+++ BookStore/DataAccessLayer/Book.cs
@@ -25,18 +25,23 @@ namespace DataAccessLayer
     
         public int ID { get; set; }
 
-        [Required(ErrorMessage = "Title can't be empty: MAX 40 Characters!")]
+        [Required(ErrorMessage = "Title can't be empty")]
         public string Title { get; set; }
         public int AuthorID { get; set; }
         public int GenreID { get; set; }
+
+        [Display(Name = "Image")]
+        [NotMapped]
+        [ValidateImage]
         public string ImagePath { get; set; }
+
+        [Display(Name = "Pages")]
         public Nullable<int> PageCount { get; set; }
         public string Description { get; set; }
         public int CountryID { get; set; }
-        
+
         [Required(ErrorMessage = "Price can't be empty!")]
         [DataType(DataType.Currency)]
-        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Enter Price for this format $XX.XX")]
         public decimal Price { get; set; }
     
         public virtual Author Author { get; set; }
